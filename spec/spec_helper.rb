fbeb00fc9c977d2e9e824e7280d306dc807fbf4c
Danny Guinther <dannyguinther@gmail.com>
2024-04-14 12:17:06 -0400

    Don't make spec helper start redis instances; assume they already exist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,30 +8,7 @@ RSpec.configure do |config|
   end
 end
 
-require 'coveralls'
-Coveralls.wear!
-
 require 'qmore'
-
-redis_configs_directory = File.join(File.dirname(File.expand_path(__FILE__)), "redis")
-redis_configs = Dir.entries(redis_configs_directory).select{|f| !File.directory?(f) && f.end_with?(".conf")}
-
-redis_configs.each do |config|
-  redis_cmd = "redis-server #{redis_configs_directory}/#{config}"
-  puts "Starting redis for testing at localhost..."
-  puts `cd #{redis_configs_directory}; #{redis_cmd}`
-
-  # Schedule the redis server for shutdown when tests are all finished.
-  at_exit do
-    redis_instance_name = config.chomp(".conf")
-    puts 'Stopping redis'
-    pid = File.read("#{redis_configs_directory}/#{redis_instance_name}.pid").to_i rescue nil
-    system ("kill -9 #{pid}") if pid.to_i != 0
-    File.delete("#{redis_configs_directory}/#{redis_instance_name}.pid") rescue nil
-    File.delete("#{redis_configs_directory}/#{redis_instance_name}-server.log") rescue nil
-    File.delete("#{redis_configs_directory}/#{redis_instance_name}-dump.rdb") rescue nil
-  end
-end
 
 def dump_redis
   result = {}
